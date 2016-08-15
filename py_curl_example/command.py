00fac91be870d7bf255e94f88e38807e99de59a6
Federico Ressi <federico.ressi@intel.com>
2016-08-15 10:19:22 +0100

    Initial commit.

---
+++ py_curl_example/command.py
@@ -0,0 +1,12 @@
+'''
+Created on 15 Aug 2016
+
+@author: fressi
+'''
+
+import sys
+
+
+def main():
+    "My script entry point."
+    sys.stderr.write('Hello!')
---
+++ py_curl_example/tests/test_py_curl.py
@@ -0,0 +1,43 @@
+'''
+Created on 15 Aug 2016
+
+@author: fressi
+'''
+import unittest
+
+import pycurl
+import six
+
+
+REQUEIRED_URL = 'http://www.oldclassiccar.co.uk/forum/phpbb/phpBB2/' + \
+                'viewtopic.php?t=12591'
+
+
+class TestPyCurl(unittest.TestCase):
+    "Test case for PyCurl library"
+
+    def test_connect_some_web_site(self):
+        "Test with connecting to pycurl with a know web page."
+        buf = six.BytesIO()
+        curl = pycurl.Curl()
+        self.addCleanup(curl.close)
+        curl.setopt(curl.URL, 'http://pycurl.io/')
+        curl.setopt(curl.WRITEDATA, buf)
+
+        curl.perform()
+        body = buf.getvalue()
+
+        self.assertTrue(body)
+
+    def test_connect_required_web_site(self):
+        "Test with connecting to pycurl with required web page."
+        buf = six.BytesIO()
+        curl = pycurl.Curl()
+        self.addCleanup(curl.close)
+        curl.setopt(curl.URL, REQUEIRED_URL)
+        curl.setopt(curl.WRITEDATA, buf)
+
+        curl.perform()
+        body = buf.getvalue()
+
+        self.assertTrue(body)
